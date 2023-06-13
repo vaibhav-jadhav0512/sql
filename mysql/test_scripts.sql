a8240187c6c5c2478edb52835f00c35322e71b4d
Vaibhav <vaibhav-jadhav0512>
2023-06-13 17:53:55 +0530

    added queries

--- mysql/test_scripts.sql
+++ mysql/test_scripts.sql
@@ -1,1 +1,4 @@
-create database sql12625809;
+USE sql12625809;
+CREATE TABLE authors (id INT, name VARCHAR(20), email VARCHAR(20));
+INSERT INTO authors (id,name,email) VALUES(1,"Vivek","xuz@abc.com");
+SELECT * FROM authors;
