10d354f1ef4467a7ea4206f5fd170f8fe685f516
Vaibhav <vaibhav-jadhav0512>
2023-06-13 18:02:28 +0530

    query

--- mysql/test_scripts.sql
+++ mysql/test_scripts.sql
@@ -1,4 +1,3 @@
 USE sql12625809;
-CREATE TABLE authors (id INT, name VARCHAR(20), email VARCHAR(20));
-INSERT INTO authors (id,name,email) VALUES(1,"Vivek","xuz@abc.com");
+INSERT INTO authors (id,name,email) VALUES(2,"Vaibhav","vaibhav@abc.com");
 SELECT * FROM authors;
